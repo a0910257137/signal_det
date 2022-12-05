2ee54571c1d5f3a6e26b4f4dcdceed8b6756d7a5
a0910257137 <a0910257137@gmail.com>
2022-12-05 09:53:09 +0800

    add votes machnism and wrap probe as class

--- probe.py
+++ probe.py
@@ -2,90 +2,75 @@ import numpy as np
 import os
 import argparse
 from tqdm import tqdm
-from matplotlib import pyplot as plt
-
 from core.base import Base
 from core.filter import Filter
-
+from box import Box
 from utils.io import load_text, load_json, parse_txt
 """
 command line:
-    python3 probe.py --config=./config/example.json
+    python3 probe.py --config=./config/probe.json
 """
 
 
-def run(cfg, src, patterns):
-    filter = Filter(cfg["filter"])
-    base = Base()
-    root_dir = os.path.split(cfg["raw_data"])[0]
-    crds_ds, tri_ds = src[0], src[1]
-    N = crds_ds.shape[0]
+class Probe:
 
-    # sparse and smooth data by using median filter
-    stride = 1
-    sim_thres, interval_per = 10., 0.5
-    eps = 1e-9
-    tri_window = 2000
-    kernel_size = patterns.shape[1]
-    normed_crds_ds = base.min_max_norm(crds_ds)
-    filted_crds_ds = filter.lowpass(normed_crds_ds)
-    np.save("/aidata/anders/objects/company/hmm/t.npy", filted_crds_ds)
-    xxx
-    # filted_src = median_filter(filted_src, r=4)
-    steps = (filted_crds_ds.shape[-1] - kernel_size + 1) / stride
-    steps += 1  # remainder
-    sim_tmp, poss_intervals = [], []
-    windowed_pattern = base.min_max_norm(patterns)
-    proj_pattern = base.proj2normal(windowed_pattern)
+    def __init__(self, cfg):
+        self.cfg = cfg
+        self.filter = Filter(cfg["filter"])
+        self.base = Base()
+        self.stride = self.cfg.window_stride
 
-    std_pattern = (windowed_pattern -
-                   np.mean(windowed_pattern, axis=-1, keepdims=True))
+        self.sim_thres = self.cfg.similarity_threshold
+        self.vote_thres = self.cfg.vote_threshold
+        self.tri_window = self.cfg.triangle_window
 
-    tri_peak_idxs = []
-    for i in tqdm(range(int(steps))):
-        if ((i + 2) * tri_window) < tri_ds.shape[-1]:
-            _, peak_idx = base.search_tri_cyc(i, tri_ds, tri_window)
-            tri_peak_idxs.append(peak_idx)
-        windowed_src = filted_crds_ds[i:kernel_size + i]
-        # last step
-        if i == steps - 1:
-            windowed_pattern = windowed_pattern[:, :np.size(windowed_src)]
-            proj_pattern = proj_pattern[:, :np.size(windowed_src)]
-            std_pattern = (windowed_pattern -
-                           np.mean(windowed_pattern, axis=-1, keepdims=True))
+    def __call__(self, raw_data, patterns):
+        time_ds, crds_ds, tri_ds = raw_data
+        N = crds_ds.shape[0]
+        eps = 1e-9
+        kernel_size = patterns.shape[1]
+        normed_crds_ds = self.base.min_max_norm(crds_ds)
+        filted_crds_ds = self.filter.lowpass(normed_crds_ds)
+        steps = (filted_crds_ds.shape[-1] - kernel_size + 1) / self.stride
+        steps += 1  # remainder
+        sim_tmp, sig_peak_idxs = [], []
+        windowed_pattern = self.base.min_max_norm(patterns)
+        proj_pattern = self.base.proj2normal(windowed_pattern)
 
-        windowed_src = base.min_max_norm(windowed_src)
-        proj_src = base.proj2normal(windowed_src[None, :])
-        curr_dist = np.sqrt(
-            np.sum(np.square(proj_src - proj_pattern), axis=-1) + eps)
-        # curr_dist = np.sqrt(
-        #     np.sum(np.square(windowed_src - windowed_pattern), axis=-1) + eps)
-        # std_src = windowed_src - np.mean(windowed_src, axis=-1, keepdims=True)
-        # curr_corr = np.sum(std_pattern * std_src, axis=-1) / np.sqrt(
-        #     np.sum(std_pattern**2, axis=-1) * np.sum(std_src**2, axis=-1))
+        tri_peak_idxs = []
+        for i in tqdm(range(int(steps))):
+            if ((i + 2) * self.tri_window) < tri_ds.shape[-1]:
+                _, peak_idx = self.base.search_tri_cyc(i, tri_ds,
+                                                       self.tri_window)
+                tri_peak_idxs.append(peak_idx)
+            windowed_src = filted_crds_ds[i:kernel_size + i]
+            if i == steps - 1:
+                windowed_pattern = windowed_pattern[:, :np.size(windowed_src)]
+                proj_pattern = proj_pattern[:, :np.size(windowed_src)]
 
-        sim_tmp.append(curr_dist)
-        if i != 0 and len(sim_tmp) % kernel_size == 0:
-            sim_tmp = np.asarray(sim_tmp).T
-            # hard-code threshold
-            mask = sim_tmp < sim_thres
-            valid_sim = np.float32(mask)
-            guessed_interval = np.sum(valid_sim, axis=-1) / kernel_size
-            votes = np.sum((guessed_interval > 0.5).astype(np.float32) /
-                           np.size(guessed_interval))
-            if votes > interval_per:
-                sig_peak_idx = np.argmax(windowed_src, axis=-1) + i
-                poss_intervals.append(sig_peak_idx)
-            sim_tmp = []
-    tri_peak_idxs = np.concatenate(tri_peak_idxs, axis=-1)
-    poss_intervals = np.asarray(poss_intervals)
-    tri_peak_idxs = tri_peak_idxs[tri_peak_idxs != -1]
-    np.save(os.path.join(root_dir, "det_idxs.npy"), poss_intervals)
-
-    print('Finish evaluating')
-    print('Totoal find %i' % len(poss_intervals))
-    print('Window size %i' % kernel_size)
-    return tri_peak_idxs, np.asarray(poss_intervals)
+            windowed_src = self.base.min_max_norm(windowed_src)
+            # core method
+            proj_src = self.base.proj2normal(windowed_src[None, :])
+            curr_dist = np.sqrt(
+                np.sum(np.square(proj_src - proj_pattern), axis=-1) + eps)
+            # curr_dist = np.sqrt(
+            #     np.sum(np.square(windowed_src - windowed_pattern), axis=-1) + eps)
+            sim_tmp.append(curr_dist)
+            if i != 0 and len(sim_tmp) % kernel_size == 0:
+                sim_tmp = np.asarray(sim_tmp).T
+                mask = sim_tmp < self.sim_thres
+                valid_sim = np.float32(mask)
+                guessed_interval = np.sum(valid_sim, axis=-1) / kernel_size
+                votes = np.sum((guessed_interval > 0.5).astype(np.float32) /
+                               np.size(guessed_interval))
+                if votes > self.vote_thres:
+                    sig_peak_idx = np.argmax(windowed_src, axis=-1) + i
+                    sig_peak_idxs.append(sig_peak_idx)
+                sim_tmp = []
+        tri_peak_idxs = np.concatenate(tri_peak_idxs, axis=-1)
+        sig_peak_idxs = np.asarray(sig_peak_idxs)
+        tri_peak_idxs = tri_peak_idxs[tri_peak_idxs != -1]
+        return tri_peak_idxs, sig_peak_idxs
 
 
 def parse_config():
@@ -95,17 +80,30 @@ def parse_config():
 
 
 if __name__ == '__main__':
+
     args = parse_config()
     print(f'Run signal detection with known patterns')
     print(f"Use the following config to produce results: {args.config}.")
-    cfg = load_json(args.config)
-
-    if os.path.isfile(cfg["pattern"]):
-        patterns = np.stack([np.load(cfg["pattern"])])
+    cfg = Box(load_json(args.config))
+    if os.path.isfile(cfg.pattern):
+        patterns = np.stack([np.load(cfg.pattern)])
     else:
         patterns = np.stack([
-            np.load(os.path.join(cfg["pattern"], p))
-            for p in os.listdir(cfg["pattern"])
+            np.load(os.path.join(cfg.pattern, p))
+            for p in os.listdir(cfg.pattern)
         ])
-    raw_data = np.load(cfg["raw_data"])
-    run(cfg, raw_data, patterns)
+    if 'txt' in cfg.raw_data.split('/')[-1]:
+        raw_data = parse_txt(load_text(cfg.raw_data))
+    elif 'npy' in cfg.raw_data.split('/')[-1]:
+        raw_data = np.load(cfg.raw_data)
+    probe = Probe(cfg)
+    tri_peak_idxs, sig_peak_idxs = probe(raw_data, patterns)
+    root_dir = os.path.split(cfg.raw_data)[0]
+
+    save_path = os.path.join(root_dir, "det_idxs.npy")
+    np.save(save_path, sig_peak_idxs)
+    print('-' * 100)
+    print('Finish evaluating')
+    print('Totoal find %i' % len(sig_peak_idxs))
+    print('Detect signal with Window %i size' % patterns.shape[-1])
+    print('save detected index in {}'.format(save_path))
